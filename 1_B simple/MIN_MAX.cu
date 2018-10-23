0244e2c1aae51025f7f98bde8e107d96ec6a238f
Shivani Shinde <31477382+erIamshivani@users.noreply.github.com>
2018-10-23 11:39:38 +0530

    Update MIN_MAX.cu

--- 1_B simple/MIN_MAX.cu
+++ 1_B simple/MIN_MAX.cu
@@ -1,81 +1,82 @@
-#include <stdio.h>
-#include <cuda.h>
-#include <stdlib.h>
-#include <time.h>
+#include<stdio.h>
+#include<cuda.h>
+#include<time.h>
 
-#define SIZE 10
-
-__global__ void max(int *c,int *a)
+#define N 8
+__global__ void minimum(int *arr,int *minVal)
 {
-	int i=threadIdx.x;
-	*a=c[0];
-	if(c[i]>*a)
-	{
-		*a=c[i];
-	}
+    int tid = threadIdx.x;
+    
+    *minVal = 999;
+    atomicMin(minVal,arr[tid]);
 }
 
-__global__ void min(int *c,int *a)
+__global__ void maximum(int *arr, int *maxVal)
 {
-	int i=threadIdx.x;
-	*a=c[0];
-	if(c[i]<*a)
-	{
-		*a=c[i];
-	}
+    int tid = threadIdx.x;
+    
+    *maxVal  = 0;
+    atomicMax(maxVal,arr[tid]);
+}
+
+__global__ void add(int *arr,int *avg) //only sums all the elements of the array
+{
+    int tid = threadIdx.x;
+    *avg=0;
+    atomicAdd(avg,arr[tid]);
 }
 
 int main(void)
 {
-	int i;
-	srand(time(NULL));
-	int a[SIZE],max_val,min_val;
-
-	int *dev_a,*dev_max,*dev_min;
-
-	cudaMalloc((void**)&dev_a, SIZE*sizeof(int));
-
-	cudaMalloc((void**)&dev_max,SIZE*sizeof(int));
-
-	cudaMalloc((void**)&dev_min,SIZE*sizeof(int));
-
-
-
-	for(i=0;i<SIZE;i++)
-	{
-
-		a[i] = rand()%20+1;
-	}
-
-	printf("\nVector is:\n");
-	for(i=0;i<SIZE;i++)
-	{
-		printf("%d  ",a[i]);
-	}
-
-	cudaMemcpy(dev_a,a,sizeof(a),cudaMemcpyHostToDevice);
-	max<<<1,SIZE>>>(dev_a,dev_max);
-	cudaMemcpy(&max_val,dev_max,sizeof(max_val),cudaMemcpyDeviceToHost);
-
-	printf("\n\n");
-    printf("The maximum value of result vector is:- %d",max_val);
-
-    cudaMemcpy(dev_a,a,sizeof(a),cudaMemcpyHostToDevice);
-    min<<<1,SIZE>>>(dev_a,dev_min);
-    cudaMemcpy(&min_val,dev_min,sizeof(min_val),cudaMemcpyDeviceToHost);
-
-    	printf("\n\n");
-        printf("The minimum value of result vector is:- %d",min_val);
-
-	return 0;
+    srand(time(NULL));
+    int i;
+    int arr[N],minVal,maxVal,addVal;
+    
+    int *dev_arr,*dev_min,*dev_max,*dev_add;
+    
+    cudaMalloc((void **)&dev_arr, N*sizeof(int));
+    cudaMalloc((void **)&dev_min, N*sizeof(int));
+    cudaMalloc((void **)&dev_max, N*sizeof(int));
+    cudaMalloc((void **)&dev_add, N*sizeof(int));
+    
+    for(i=0;i<N;i++)
+    {
+        arr[i] = rand()%20 + 1;
+    }
+    
+    printf("The array is : ");
+   
+    for(i=0;i<N;i++)
+    {
+        printf(" %d ",arr[i]);
+    }
+    
+    cudaMemcpy(dev_arr,arr,sizeof(arr),cudaMemcpyHostToDevice);
+    
+    minimum<<<1,N>>>(dev_arr,dev_min);
+    
+    cudaMemcpy(&minVal,dev_min,sizeof(minVal),cudaMemcpyDeviceToHost);
+    
+    printf("The minimum value is : %d",minVal);
+    
+    
+    
+    cudaMemcpy(dev_arr,arr,sizeof(arr),cudaMemcpyHostToDevice);
+    
+    maximum<<<1,N>>>(dev_arr,dev_max);
+    
+    cudaMemcpy(&maxVal,dev_max,sizeof(maxVal),cudaMemcpyDeviceToHost);
+    
+    printf("The maximum value is : %d",maxVal);
+    
+    
+    cudaMemcpy(dev_arr,arr,sizeof(arr),cudaMemcpyHostToDevice);
+    
+    add<<<1,N>>>(dev_arr,dev_add);
+    
+    cudaMemcpy(&addVal,dev_add,sizeof(addVal),cudaMemcpyDeviceToHost);
+    
+    printf("The average value is : %d",addVal/N);
+    
+    return 0;
 }
-/*
-
-Vector is:
-7  6  7  2  18  8  17  13  16  19
-
-The maximum value of result vector is:- 19
-
-The minimum value of result vector is:- 2
-
- */
